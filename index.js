1db134fb19c455ffc91f277099c7d1364e25c22b
fuxingjun <fuxingjun@qq.com>
2021-11-05 15:23:27 +0800

    修改入口2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2021-11-05 15:04:35
+ * @LastEditTime: 2021-11-05 15:20:24
  * @Description: entry
  */
 
@@ -9,10 +9,12 @@ const logger = require("./utils/logger");
 
 (async () => {
     const portList = [
-        { subdomain: "qmyxpay2", port: 8078, local_host: "http://192.168.20.132" },
+        // { port: 5901, subdomain: "novnc", local_host: "localhost" },
+        // { port: 18022, subdomain: "d-linux" },
+        { subdomain: "qmyxpay2", port: 8078, local_host: "192.168.20.132" },
     ];
     portList.forEach(item => {
-        localtunnel({ port: item.port, subdomain: item.subdomain }).then(tunnel => {
+        localtunnel({ port: item.port, subdomain: item.subdomain, local_host: item.local_host }).then(tunnel => {
             // the assigned public url for your tunnel
             // i.e. https://abcdefgjhij.localtunnel.me
             logger.info(`expose ${item.local_host}:${item.port} successful! the url is "${tunnel.url}"`)
